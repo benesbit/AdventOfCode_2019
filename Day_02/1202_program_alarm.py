c09c54fa0805e5a7ea909384a1961ed9788e9411
xDOASx <benesbit@yahoo.com>
2020-01-02 14:08:04 -0800

    [1202_program_alarm.py] Add function to help with "opt code" logic.

--- Day_02/1202_program_alarm.py
+++ Day_02/1202_program_alarm.py
@@ -1,7 +1,20 @@
 # 1202_program_alarm.py
 
+def continue_program(code):
+    if (code == 99):
+        print("Code <99> encountered. Ending program")
+        return False
+    elif (code != 1 and code != 2):
+        print("Error!")
+        print("Unrecognized code: <" + str(code) + "> encountered. Ending program.")
+        return False
+    return True
+
 def run_intcode(i_code):
-    print(i_code)
+    position = 0
+
+    while continue_program(i_code[position]):
+        print(i_code)
 
 Intcode = [1,9,10,3,2,3,11,0,99,30,40,50]
 
